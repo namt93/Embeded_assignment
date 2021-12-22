276bf1a7d2259de68615c7a6ce22f5218ebf703d
KhanhNgoDuy <94754952+KhanhNgoDuy@users.noreply.github.com>
2021-12-22 23:54:07 +0700

    Update error.cpp

--- error.cpp
+++ error.cpp
@@ -23,29 +23,29 @@ void err_textF(const char *inp, int n) {
     if (myFile.is_open()) {
         while (getline(myFile, line)) {
             line_counter ++;        //Count the number of lines in the input file.
-            line += '\n';
+            line += '\n';       //Add '\n' to the end of every lines
             for (int i = 0; i < line.length(); i++) {
                 line[i] = tolower(line[i]);        //Modify the input data
                 s += line[i];       //Read the input data
 
-                if (line[i] != ' ' && line[i] != '\n') {
+                if (line[i] != ' ' && line[i] != '\n') {        //Start detecting error if line[i] is not a seperator
                     for (int j = 0; j < size_of_array; j++) {
-                        if (s == letter[j]) {       
+                        if (s == letter[j]) {       //Check if "s" does exist in the "letter" array
                             j = size_of_array;      //The case of
                             trChar++;               //successful translation.
                         }
-                        else if (j == (size_of_array - 1)) {
+                        else if (j == (size_of_array - 1)) {        //The case of "s" not being in the "letter" array
                             cout<<"Error T1: Unrecognize character "<<line[i]<<" on line "<<line_counter<<".\n";
                             line1 += line[i];
                             line_num[numl++] = line_counter;
                             errChar++;      
-                            err_bool = 1;
+                            err_bool = 1;       //The word has error(s)
                         }
                     }
                 }
-                else if ((line[i] == ' ' || line[i] == '\n') && err_bool == 1) {
-                    errWord++;          //Check the error
-                    err_bool = 0;       //of each words.
+                else if ((line[i] == ' ' || line[i] == '\n') && err_bool == 1) {        //Check if the word has error or not
+                    errWord++;          //If err_bool = 1
+                    err_bool = 0;       //then the word has error(s)
                 };
                 s = "";
             }
@@ -68,16 +68,16 @@ void err_morse(const char *inp) {
             line_counter++;
             line += '\n';
             for (int i = 0; i < line.length(); i++) {
-                if (line[i] == '.' || line[i] == '-') {
+                if (line[i] == '.' || line[i] == '-') {         //Append line[i] to s_temp if it's not a seperator
                     s_temp += line[i];          //Read each Morse characters.
                     space_counter = 0;          
                 }
 
-                else if (line[i] == '/' || line[i] == ' ' || line[i] == '\n') {
+                else if (line[i] == '/' || line[i] == ' ' || line[i] == '\n') {     //Start checking error
                     if (line[i] == ' ') {
                         space_counter++;
 
-                        if (space_counter == 2) {
+                        if (space_counter == 2) {       //The case of having 2 ' ' between each characters
                             space_bool ++;
                             extBlank[line_counter]++;
                             errMorse++;
@@ -91,7 +91,7 @@ void err_morse(const char *inp) {
                     };
 
                     for (int j = 0; j < size_of_array; j++) {
-                        if (s_temp.length() > 7) {
+                        if (s_temp.length() > 7) {      //The case of word's length being larger than 7
                             cout<<"Error M2: Invalid Morse code "<<s_temp<<" on line "<<line_counter<<".\n";
                             line1 += s_temp;
                             line_num[numl++] = line_counter;
@@ -102,7 +102,7 @@ void err_morse(const char *inp) {
                             err_bool = 1;
                         }
 
-                        else if (s_temp != morse[j] && j == (size_of_array - 1) ) {
+                        else if (s_temp != morse[j] && j == (size_of_array - 1) ) {     //The case of s_temp not being in the "morse" array 
                                 cout<< "Error M3: Invalid Morse code "<<s_temp<<" on line "<<line_counter<<".\n";
                                 line1 += s_temp;
                                 line_num[numl++] = line_counter;
