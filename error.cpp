407ce9f4a4b3b93d4b59af47699bede0547ebf6c
namt93 <namt7115633@gmail.com>
2021-12-22 21:32:38 +0700

    update comments

--- error.cpp
+++ error.cpp
@@ -6,10 +6,11 @@
 
 using namespace std;
 
-int trChar = 0, errWord = 0;     //Counter for translated characters and words.
-int errChar = 0, errMorse = 0;   //Counter for error characters.
-string line1 = "";
-int line_num[200], w_length[200];
+int trChar = 0, errWord = 0;        /* Counter for translated characters and words. */
+int errChar = 0, errMorse = 0;      /* Counter for error characters. */
+string line1 = "";                  /* Get unrecognized characters. */
+int line_num[200];
+int w_length[200];                  /* Get the length of unrecognized morse codes. */
 int numl = 0;
 
 void err_textF(const char *inp, int n) {
@@ -34,7 +35,7 @@ void err_textF(const char *inp, int n) {
                             trChar++;               //successful translation.
                         }
                         else if (j == (size_of_array - 1)) {
-                            cout<<"Error T1: Unrecognize character "<<line[i]<<" on line "<<line_counter<<"\n";
+                            cout<<"Error T1: Unrecognize character "<<line[i]<<" on line "<<line_counter<<".\n";
                             line1 += line[i];
                             line_num[numl++] = line_counter;
                             errChar++;      
@@ -53,10 +54,12 @@ void err_textF(const char *inp, int n) {
     }
 }
 
+int line_counter;
+int extBlank[2000]; /* Counter for the number of extra blanks */
 void err_morse(const char *inp) {
     fstream myFile;
     string line, s_temp = "";
-    int line_counter = 0, space_counter = 0, err_bool = 0;
+    int space_counter = 0, err_bool = 0, space_bool = 0;
     int j = 0, numlf = 0;
 
     myFile.open(inp, ios::in);
@@ -75,7 +78,8 @@ void err_morse(const char *inp) {
                         space_counter++;
 
                         if (space_counter == 2) {
-                            cout<<"Error M1: Extra blankspace on line "<<line_counter<<"\n";
+                            space_bool ++;
+                            extBlank[line_counter]++;
                             errMorse++;
                             space_counter = 0; 
                             err_bool = 1;
@@ -88,7 +92,7 @@ void err_morse(const char *inp) {
 
                     for (int j = 0; j < size_of_array; j++) {
                         if (s_temp.length() > 7) {
-                            cout<<"Error M2: Invalid Morse code "<<s_temp<<" on line "<<line_counter<<"\n";
+                            cout<<"Error M2: Invalid Morse code "<<s_temp<<" on line "<<line_counter<<".\n";
                             line1 += s_temp;
                             line_num[numl++] = line_counter;
                             w_length[numlf++] = s_temp.length();
@@ -99,7 +103,7 @@ void err_morse(const char *inp) {
                         }
 
                         else if (s_temp != morse[j] && j == (size_of_array - 1) ) {
-                                cout<< "Error M3: Invalid Morse code "<<s_temp<<" on line "<<line_counter<<"\n";
+                                cout<< "Error M3: Invalid Morse code "<<s_temp<<" on line "<<line_counter<<".\n";
                                 line1 += s_temp;
                                 line_num[numl++] = line_counter;
                                 w_length[numlf++] = s_temp.length();
@@ -120,10 +124,12 @@ void err_morse(const char *inp) {
                     };
                 };
             };
-
             s_temp = "";
         };
-
+        for (int i = 0; i < line_counter + 1; i++){
+                if (extBlank[i] > 0)
+                    cout<<"Error M1: "<< extBlank[i] <<" extra blankspaces on line "<<i<<".\n"; 
+            }
         myFile.close();
     };
 };
